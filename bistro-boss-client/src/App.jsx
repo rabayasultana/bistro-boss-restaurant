38fae7eef6ac5949c29d3f9e70c88893ddbe4e46
RabayaSultana <rabayashova811@gmail.com>
2024-08-15 10:31:09 +0600

    first commit

---
+++ bistro-boss-client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>started</h1>
+    </>
+  )
+}
+
+export default App
---
+++ bistro-boss-client/src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import { Helmet } from "react-helmet-async";
+import Banner from "./Banner";
+import Category from "./Category";
+import Featured from "./Featured";
+import PopularMenu from "./PopularMenu";
+import Testimonials from "./Testimonials";
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <Category></Category>
+      <PopularMenu></PopularMenu>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
+    </div>
+  );
+};
+
+export default Home;

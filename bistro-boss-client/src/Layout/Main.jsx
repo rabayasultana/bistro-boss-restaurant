56966eb50ebcbca6c33a33cfbf0e4de11bae8e65
RabayaSultana <rabayashova811@gmail.com>
2024-08-15 19:03:54 +0600

    Login page started

--- bistro-boss-client/src/Layout/Main.jsx
+++ bistro-boss-client/src/Layout/Main.jsx
@@ -1,13 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/shared/Footer";
 import Navbar from "../components/shared/Navbar";
 
 const Main = () => {
+    const location = useLocation();
+    console.log(location);
+    const noHeaderFooter = location.pathname.includes('login');
     return (
         <div>
-            <Navbar></Navbar>
+            { noHeaderFooter || <Navbar></Navbar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            { noHeaderFooter || <Footer></Footer>}
         </div>
     );
 };

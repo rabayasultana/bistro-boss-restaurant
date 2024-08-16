7b889a916c8c314eafee78f4732a5b2a49f95587
RabayaSultana <rabayashova811@gmail.com>
2024-08-16 22:52:44 +0600

    login page done

--- bistro-boss-client/src/components/shared/Navbar.jsx
+++ bistro-boss-client/src/components/shared/Navbar.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Navbar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+        .then(() => { })
+        .catch(error => console.log(error));
+}
+
   const navOptions = (
     <>
       <li>
@@ -12,9 +22,15 @@ const Navbar = () => {
       <li>
         <Link to="/order/salad">Order Food</Link>
       </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
+      
+      {
+            user ? <>
+                <span>{user?.displayName}</span>
+                <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
+            </> : <>
+                <li><Link to="/login">Login</Link></li>
+            </>
+        }
       
     </>
   );

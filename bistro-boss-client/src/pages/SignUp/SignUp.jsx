f0baab5f247a2c2f85fc6b1468c6a57b50a53f7e
RabayaSultana <rabayashova811@gmail.com>
2024-08-16 22:43:26 +0600

    User created

--- bistro-boss-client/src/pages/SignUp/SignUp.jsx
+++ bistro-boss-client/src/pages/SignUp/SignUp.jsx
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const SignUp = () => {
 
-    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile } = useContext(AuthContext);
 
     const onSubmit = data => {
         console.log(data);
+        createUser(data.email, data.password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+            })
     };
 
 

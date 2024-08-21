e9a806b1ad1f4886d932b39298d57e473eed5e90
RabayaSultana <rabayashova811@gmail.com>
2024-08-21 12:43:50 +0600

    Save Cart data to the server using axios

--- bistro-boss-client/src/components/shared/FoodCard.jsx
+++ bistro-boss-client/src/components/shared/FoodCard.jsx
@@ -1,35 +1,62 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import axios from "axios";
 
-const FoodCard = ({item}) => {
-  const { name, image, price, recipe } = item;
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe, _id } = item;
   const { user } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleAddToCart = food => {
+  const handleAddToCart = (food) => {
     if (user && user.email) {
-// todo
+      //send cart item to the database
+      console.log(user.email, food);
+      //send cart item to the database
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        image,
+        price
     }
+    // axiosSecure.post('/carts', cartItem)
+    axios.post('http://localhost:5000/carts', cartItem)
+    .then(res => {
+        console.log(res.data)
+        if (res.data.insertedId) {
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: `${name} added to your cart`,
+                showConfirmButton: false,
+                timer: 2000
+            });
+        //     // refetch cart to update the cart items count
+        //     refetch();
+        }
 
+    })
+    } 
+    
     else {
       Swal.fire({
-          title: "You are not Logged In",
-          text: "Please login to add to the cart?",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, login!"
+        title: "You are not Logged In",
+        text: "Please login to add to the cart?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, login!",
       }).then((result) => {
-          if (result.isConfirmed) {
-              //   send the user to the login page
-              // navigate('/login', { state: { from: location } })
-              navigate('/login')
-          }
+        if (result.isConfirmed) {
+          //   send the user to the login page
+          navigate("/login", { state: { from: location } });
+        }
       });
     }
-  }
+  };
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -42,7 +69,10 @@ const FoodCard = ({item}) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button onClick= {() =>handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4"
+          >
             Add to Cart
           </button>
         </div>

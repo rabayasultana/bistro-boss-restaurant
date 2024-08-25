8cfd08f5c8d30bae3dbf4f3c3da4bd324ece76e1
RabayaSultana <rabayashova811@gmail.com>
2024-08-26 00:45:18 +0600

    Logout unauthorized access and check is admin

--- bistro-boss-client/src/hooks/useAxiosSecure.jsx
+++ bistro-boss-client/src/hooks/useAxiosSecure.jsx
@@ -1,14 +1,20 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
+
+    const navigate = useNavigate();
+    const { logOut } = useAuth();
+
    // request interceptor to add authorization header for every secure call to teh api
    axiosSecure.interceptors.request.use(function (config) {
     const token = localStorage.getItem('access-token')
-    console.log('request stopped by interceptors', token)
+    // console.log('request stopped by interceptors', token)
     config.headers.authorization = `Bearer ${token}`;
     return config;
 }, function (error) {
@@ -22,12 +28,12 @@ const useAxiosSecure = () => {
         return response;
     }, async (error) => {
         const status = error.response.status;
-        console.log('status error in the interceptor', status);
+        // console.log('status error in the interceptor', status);
         // for 401 or 403 logout the user and move the user to the login
-        // if (status === 401 || status === 403) {
-        //     await logOut();
-        //     navigate('/login');
-        // }
+        if (status === 401 || status === 403) {
+            await logOut();
+            navigate('/login');
+        }
         return Promise.reject(error);
     })
 

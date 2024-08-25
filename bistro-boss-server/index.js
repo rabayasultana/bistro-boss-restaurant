148cfce857ecd333450eafbefe52900dc3769fb5
RabayaSultana <rabayashova811@gmail.com>
2024-08-26 00:05:20 +0600

    Create a JWT token and save it on local storage

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -32,12 +33,36 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+     // jwt related api
+     app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    });
+
+      // middlewares 
+      const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers);
+        // console.log('inside verify token', req.headers.authorization);
+        // if (!req.headers.authorization) {
+        //   return res.status(401).send({ message: 'unauthorized access' });
+        // }
+        // const token = req.headers.authorization.split(' ')[1];
+        // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        //   if (err) {
+        //     return res.status(401).send({ message: 'unauthorized access' })
+        //   }
+        //   req.decoded = decoded;
+          next();
+        // })
+      }
+
     // users related api
     // app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
     //   const result = await userCollection.find().toArray();
     //   res.send(result);
     // });
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken,  async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });

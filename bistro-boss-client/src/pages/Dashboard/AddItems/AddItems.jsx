243ffd092578892b689af823812757ca649c3215
RabayaSultana <rabayashova811@gmail.com>
2024-09-03 12:59:53 +0600

    Upload image to image hosting server imgbb and get image url

--- bistro-boss-client/src/pages/Dashboard/AddItems/AddItems.jsx
+++ bistro-boss-client/src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,13 +1,26 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/shared/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic"
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
     // const { register, handleSubmit, reset } = useForm();
     const { register, handleSubmit } = useForm();
+    const axiosPublic = useAxiosPublic();
 
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
         console.log(data);
+         // image upload to imgbb and then get an url
+         const imageFile = { image: data.image[0] }
+         const res = await axiosPublic.post(image_hosting_api, imageFile, {
+             headers: {
+                 'content-type': 'multipart/form-data'
+             }
+         });
+         console.log(res.data);
+
     }
   return (
     <div>
